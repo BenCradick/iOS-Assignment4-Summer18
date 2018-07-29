e1c8b18171f0cf48376ae44873a1738de8d8303d
Ben Cradick <ben.cradick@gmail.com>
2018-07-29 11:45:38 -0500

    Initial Commit

---
+++ SimplyBusiness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimplyBusiness
+//
+//  Created by Ben Cradick on 7/29/18.
+//  Copyright © 2018 Ben Cradick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
